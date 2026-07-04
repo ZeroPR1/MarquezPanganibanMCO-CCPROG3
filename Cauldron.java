b448bb50538eaff02bcea56a7aa61d81fec518f7
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-07-04 16:05:07 +0800

    Implement Cauldron class with usability methods

--- Cauldron.java
+++ Cauldron.java
@@ -1,1 +1,19 @@
+public class Cauldron { //represents the brewing station
+  private boolean isUsable;
 
+  public Cauldron() { //makes a new cauldron, is usable by default ofcourse
+    this.isUsable = true;
+  }
+
+  public boolean checkUsability() {
+    return this.isUsable;
+  }
+
+  public void ruinCauldron() {//detects the cauldron as unsable after a failed brew
+    this.isUsable = false;
+  }
+
+  public void blessCauldron() { //restores the cauldron so it can be usable again
+    this.isUsable = true;
+  }  
+}
