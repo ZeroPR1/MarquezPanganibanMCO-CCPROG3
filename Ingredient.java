c0a096b66161266b47ed07833d4dc0de09c6ff7c
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-07-05 20:48:24 +0800

    Implement Ingredient class with quantity management

--- Ingredient.java
+++ Ingredient.java
@@ -1,1 +1,33 @@
+public class Ingredient{
+  private String name;
+  private int quantity;
 
+// @param name = Name of the item
+// @param quantity = starting quantity
+  
+  public Ingredient(String name, int quantity){
+    this.name = name;
+    this.quantity = quantity;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public int getQuantity(){
+    return this.quantity;
+  }
+
+  public void addQuantity(int amount){
+    if (amount > 0) this.quantity += amount;
+  }
+
+  public boolean deductQuantity(int amount){
+    if (amount > 0 && this.quantity >= amount){
+      this.quantity -= amount;
+      return true;
+    }
+    return false;
+  }
+  
+}
