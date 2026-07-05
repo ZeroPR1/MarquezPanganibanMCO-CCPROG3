5e3957037ed9940d1d72b62fc931a784ebc5b800
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-07-05 21:26:50 +0800

    Create IngredientSlot class with fields
    Added IngredientSlot class with itemName and quantity fields.

--- IngredientSlot.java
+++ IngredientSlot.java
@@ -1,1 +1,7 @@
+//note: kyle marquezs' laptop was not turning on, in turn, he dictated what to type through discord calls and messenger chats
+public class IngredientSlot {
+  private String itemName;
+  private int quantity;
 
+  
+}
