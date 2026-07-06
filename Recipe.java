78d233bb23e1283d560e3ac69820d37e01c28e23
XavMarq <kyle_xavier_marquez@dlsu.edu.ph>
2026-07-06 17:03:21 +0800

    Create Recipe class with fields

--- Recipe.java
+++ Recipe.java
@@ -1,1 +1,9 @@
+import java.util.ArrayList;
 
+public class Recipe {
+  private int concoctionId;
+  private String recipeName;
+  private String baseName;
+  private ArrayList<String> requiredFruits;
+
+}
