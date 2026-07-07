302e192104a76ba57c1fc9f162cca07a670b3316
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-07-07 13:11:19 +0800

    Refactor Inventory class structure and constructor
    Refactored Inventory class to improve readability and initialization.

--- Inventory.java
+++ Inventory.java
@@ -1,7 +1,16 @@
 import java.util.ArrayList;
 
-public class Inventory{
+public class Inventory {
   private ArrayList<Ingredient> fruits;
   private ArrayList<Ingredient> bases;
   private ArrayList<Cauldron> cauldrons;
+
+  public Inventory() {
+    this.fruits = new ArrayList<Ingredient>();
+    this.bases = new ArrayList<Ingredient>();
+    this.cauldrons = new ArrayList<Cauldron>();
+    for(int i = 0; i < 3; i++){
+      
+    }
+  }
 }
