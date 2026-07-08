7211b17c7e15a00dfc842c019bae1748004a547e
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-07-08 20:32:35 +0800

    Implement add and remove methods for fruits and bases
    Added methods to manage fruits and bases in the inventory.

--- Inventory.java
+++ Inventory.java
@@ -10,7 +10,36 @@ public class Inventory {
     this.bases = new ArrayList<Ingredient>();
     this.cauldrons = new ArrayList<Cauldron>();
     for(int i = 0; i < 3; i++){
-      
+        this.cauldrons.add(new Cauldron());
+    }
+
+    public void addFruit(String name, int quantity){
+      for (int i = 0; i < this.fruits.size(); i++){
+          if (this.fruits.get(i).getname().equals(name)){
+              this.fruits.get(i).addQuantity(quantity);
+              return;
+          }
+      }
+      this.fruits.add(new Ingredient(name, quantity));
+    }
+
+    public boolean removeFruit(String name, int quantity){
+        for (int i = 0; i < this.fruits.size(); i++){
+          if (this.fruits.get(i).getName().equals(name)){
+              return this.fruits.get(i).deductQuantity(quantity);
+          }
+        }
+        return false;
+    }
+
+    public void addBase(String name, int quantity){
+        for (int i = 0; i < this.bases.size(); i++){
+            if (this.bases.get(i).getName().equals(name)){
+                this.bases.get(i).addQuantity(quantity);
+                return;
+            }
+        }
+        this.bases.add(new Ingredient(name, quantity));
     }
   }
 }
