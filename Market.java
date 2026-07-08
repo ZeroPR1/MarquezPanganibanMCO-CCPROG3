59432723074096218e54d72905c50c7dde682db3
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-07-08 20:37:55 +0800

    Implement Market class with constructor and refresh method
    Initialize Market class with slots and cauldron stock.

--- Market.java
+++ Market.java
@@ -1,1 +1,18 @@
+import java.util.Random;
 
+public class Market {
+    private IngredientSlot[] slots;
+    private boolean cauldronInStock;
+
+    public Market(){
+      this.slots = new IngredientSlot[8]
+      refreshMarket();
+    }
+
+    public void RefreshMarket(){
+      Random rand = new Random();
+      this.cauldronInStock = false;
+      String[] possibleItems = {}
+    }
+  
+}
