ddab88b6296677ac184580d892b68e086ab149b8
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-07-09 18:44:35 +0800

    Add Spellbook class with recipe management
    Implemented a Spellbook class with functionality to add and check recipes.

--- Spellbook.java
+++ Spellbook.java
@@ -1,1 +1,26 @@
 import java.util.ArrayList;
+
+public class Spellbook{
+  private ArrayList<Recipe> unlockedRecipes;
+
+  public Spellbook() {
+    this.unlockedRecipes = new ArrayList<Recipe>();
+  }
+
+  public void addRecipe(Recipe newRecipe){ //adds a recipe to the spell book if it wasnt already unlocked
+    if (!hasRecipe(newRecipe.getId())){
+      this.unlockedRecipes.add(newRecipe);
+      System.out.println("Alchemy Success! New recipe added to spellbook: " + newRecipe.getName());
+    }
+  }
+
+  public boolean hasRecipe(String id) {
+    for (int i = 0; i < this.unlockedRecipes.size(); i++){
+      if (this.unlockedRecipes.get(i).getId().equals(id)){
+        return true;
+      }
+    }
+    return false;
+  }
+  
+}
