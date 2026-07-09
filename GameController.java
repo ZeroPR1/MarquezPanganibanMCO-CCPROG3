c5aba74e6df43f75e8f06ab723e73c25ce576395
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-07-09 14:31:45 +0800

    Add imports for file handling and random generation

--- GameController.java
+++ GameController.java
@@ -1,1 +1,7 @@
-
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
