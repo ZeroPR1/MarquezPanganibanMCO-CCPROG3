1d699f51c96392403b0d9b0524d8afc7fddedb7a
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-07-09 19:29:36 +0800

    Add GameController class with initial fields

--- GameController.java
+++ GameController.java
@@ -5,3 +5,14 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Random;
+
+public class GameController {
+  private Player currentPlayer;
+  private Market market;
+  private ArrayList<Recipe> recipeCompendium;
+  private boolean loginBonusClaimed;
+  private int brewsSinceMarket;
+  private Scanner scanner;
+
+  
+}
