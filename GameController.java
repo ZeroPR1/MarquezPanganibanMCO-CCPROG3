a17df865900c27d1070c6fba159c30d4535fb445
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-07-09 21:38:44 +0800

    Update GameController.java

--- GameController.java
+++ GameController.java
@@ -24,7 +24,24 @@ public class GameController {
   }
 
   private void loadCompendium() { //darshan
-    
+      try {
+          File file = new File("POTION COMPENDIUM.csv");
+          Scanner fileScanner = new Scanner(file);
+          while (fileScanner.hasNextLine()) {
+              String[] data = fileScanner.nextLine().split(",", -1);
+              if (data.length >= 4) {
+                  Recipe r = new Recipe(data[0], data[1], data[2], Integer.parseInt(data[3]));
+                  if (data.length > 4 && !data[4].isEmpty()) { r.addFruitRequirement(data[4]); }
+                  if (data.length > 5 && !data[5].isEmpty()) { r.addFruitRequirement(data[5]); }
+                  if (data.length > 6 && !data[6].isEmpty()) { r.addFruitRequirement(data[6]); }
+                  this.recipeCompendium.add(r);
+              }
+          }
+          fileScanner.close();
+      }  
+      catch (Exception e) {
+        System.out.println("Warning: POTION COMPENDIUM.csv not found.");    
+      }
   }
 
   public void startGame() { //kyle
