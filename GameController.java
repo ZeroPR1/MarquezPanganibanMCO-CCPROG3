e9504a98015d63c36cc3abe85713c0879426c6b8
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-07-09 22:14:26 +0800

    Update GameController.java

--- GameController.java
+++ GameController.java
@@ -61,7 +61,60 @@ public class GameController {
   }
 
   private void recipeMode() { //darshan
-    
+      if (currentPlayer.getInventory().getUsableCauldronCount() <= 1){
+          System.out.println("Error: You only have 1 usable cauldron left. Creative mode locked to prevent soft-lock");
+          return;
+      }
+
+      System.out.print("Enter Concotion Base: ");
+      String base = scanner.nextLine().toUpperCase();
+      if (!currentPlayer.getInventory().checkIngredientAvailability(base, 1, true)){
+          System.out.println("Error: Insufficient base.");
+          return;
+      }
+
+      System.out.print("Enter fruits (comma seperated, max 3): ");
+      String[] fruits = scanner.nextLine().toUpperCase().split(",");
+      ArrayList<String> fruitList = new ArrayList<>();
+
+      for (int i = 0; i < fruits.length && i < 3; i++) {
+          String f = fruits[i].trim();
+          if (fruitList.contains(f)) {
+              System.out.println("Error: cannot repeat ingredients.");
+              return;
+          }
+          if (!currentPlayer.getInventory().checkIngredientAvailability(f, 1, false)){
+              System.out.println("Error: Insufficient " + f + ".");
+              return;
+          }
+          fruitList.add(f);
+      }
+
+      System.out.print("Confirm brew? (Y/N): ");
+      if (scanner.nextLine().equalsIgnoreCase("Y)) {
+          currentPlayer.getInventory().removeBase(base, 1);
+          for(int i = 0; i < fruitList.size(); i++) {
+            currentPlayer.getInventory().removeFruit(fruitList.get(i), quantity: 1);
+          }
+
+          boolean success = false;
+          for (int i = 0; i < recipeCompendium.size() && !success; i++) {
+              if (recipeCompendium.get(i).matchesIngredients(base, fruitList)) {
+                  success = true;
+                  Recipe r = recipeCompendium.get(i);
+                  System.out.println("Success! Brewed " + r.getName() + "!");
+                  currentPlayer.addCrystals(r.getPrice());
+                  currentPlayer.getSpellbook().addRecipe(r);
+                  brewsSinceMarket++;
+              }
+          }
+
+        if (!success) {
+            System.out.println("Failure! the mixture exploded D: cauldron ruined :<");
+            currentPlayer.getInventory().ruinOneCauldron();
+            brewsSinceMarket++;
+        }
+      }   
   }
 
   private int getIngredientPrice(String name) {//kyle
