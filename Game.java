fdc38d3117b754b29c9250f03e502f86f2f461fb
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-07-10 21:33:39 +0800

    Add constructor to Game class

--- Game.java
+++ Game.java
@@ -3,9 +3,15 @@
  */
 
 public class Game {
-
+	
+	public Game() {
+		
+	}
+	/**
+     * The main entry point for the application.
+     * * @param args Command line arguments (not used).
+     */
   public static void main(String[] args){
-
     GameController gameController = new GameController();
     gameController.startGame();
   }
