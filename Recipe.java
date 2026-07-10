c2369acb7a3f2e97914ecc25076056e9032bbb86
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-07-10 21:35:54 +0800

    Fix HTML tags in Javadoc comments

--- Recipe.java
+++ Recipe.java
@@ -34,7 +34,7 @@ public class Recipe {
    * @param baseName The name of the required base ingredient.
    * @param price The integer selling price of the potion.
    * <p><b>Pre-conditions:</b> All string parameters must be valid non-null Strings.</p>
-   * <p><b>Post-conditions:<b> A new Recipe object is instantiated and the requiredFruits list is initialized.</p>
+   * <p><b>Post-conditions:</b> A new Recipe object is instantiated and the requiredFruits list is initialized.</p>
    */
   public Recipe(int concoctionId, String recipeName, String baseName, int price) {
     this.concoctionId = concoctionId;
@@ -48,7 +48,7 @@ public class Recipe {
    * Adds a specified fruit to the list of required ingredients for this recipe.
    * @param fruitName The name of the fruit to add.
    * <p><b>Pre-conditions:</b> fruitName must be a valid non-null String.</p>
-   * <p><b>Post-conditions:<b> The fruit name is appended to the requiredFruits list.</p>
+   * <p><b>Post-conditions:</b> The fruit name is appended to the requiredFruits list.</p>
    */
   public void addRequiredFruit(String fruitName) {
     this.requiredFruits.add(fruitName);
@@ -58,7 +58,7 @@ public class Recipe {
    * Retrieves the unique identifier of the recipe.
    * @return The integer ID of the recipe.
    * <p><b>Pre-conditions:</b> None.</p>
-   * <p><b>Post-conditions:<b> The concoctionId attribute is returned.</p>
+   * <p><b>Post-conditions:</b> The concoctionId attribute is returned.</p>
    */
   public int getId() {
     return this.concoctionId;
@@ -68,7 +68,7 @@ public class Recipe {
    * Retrieves the required base ingredient of the recipe.
    * @return The String name of the base ingredient.
    * <p><b>Pre-conditions:</b> None.</p>
-   * <p><b>Post-conditions:<b> The baseName attribute is returned.</p>
+   * <p><b>Post-conditions:</b> The baseName attribute is returned.</p>
    */
   public String getBaseName(){
     return this.baseName;
@@ -78,7 +78,7 @@ public class Recipe {
    * Retrieves the name of the completed potion.
    * @return The String name of the recipe.
    * <p><b>Pre-conditions:</b> None.</p>
-   * <p><b>Post-conditions:<b> The recipeName attribute is returned.</p>
+   * <p><b>Post-conditions:</b> The recipeName attribute is returned.</p>
    */
   public String getName() {
     return this.recipeName;
@@ -88,7 +88,7 @@ public class Recipe {
    * Retrieves the list of fruits required to brew this recipe.
    * @return An ArrayList containing the names of the required fruits.
    * <p><b>Pre-conditions:</b> None.</p>
-   * <p><b>Post-conditions:<b> The requiredFruits ArrayList is returned.</p>
+   * <p><b>Post-conditions:</b> The requiredFruits ArrayList is returned.</p>
    */
   public ArrayList<String> getRequiredFruits() {
     return this.requiredFruits;
@@ -98,7 +98,7 @@ public class Recipe {
    * Retrieves the selling price of the completed potion.
    * @return The integer price of the recipe.
    * <p><b>Pre-conditions:</b> None.</p>
-   * <p><b>Post-conditions:<b> The price attribute is returned.</p>
+   * <p><b>Post-conditions:</b> The price attribute is returned.</p>
    */
   public int getPrice() {
     return this.price;
@@ -110,7 +110,7 @@ public class Recipe {
    * @param inputFruits A list of fruit names provided by the player.
    * @return True if the ingredients perfectly match the recipe, false otherwise.
    * <p><b>Pre-conditions:</b> inputBase is a valid String and inputFruits is a valid ArrayList.</p>
-   * <p><b>Post-conditions:<b> Returns true if the base matches and the fruits array contains all 
+   * <p><b>Post-conditions:</b> Returns true if the base matches and the fruits array contains all 
    * required items without evaluating state changes.</p>
    */
   public boolean matchesIngredients(String inputBase, ArrayList<String> inputFruits) {
