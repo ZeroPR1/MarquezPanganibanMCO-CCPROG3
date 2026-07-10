66bde325724ea1b61648b64538b253d39204a0b9
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-07-10 14:07:03 +0800

    Enhance documentation for Cauldron class
    Added detailed JavaDoc comments for the Cauldron class and its methods to improve documentation and clarify functionality.

--- Cauldron.java
+++ Cauldron.java
@@ -1,19 +1,53 @@
-public class Cauldron { //represents the brewing station
+  /**
+   * Cauldron.java
+   * This file contains the cauldron class, which represents a brewing station
+   * in the game. It manages the operational state of the cauldron for crafting.
+   */ 
+
+  /**
+   * Represents a brewing station used by players to create potions or spells.
+   * This class tracks wheter the cauldron is currently in a usable state
+   * or if it requires restoration after a failed brewing attempt.
+   */ 
+public class Cauldron {
+  /** Tracks the current operational status of the cauldron (true if usable, false if ruined). */
   private boolean isUsable;
 
-  public Cauldron() { //makes a new cauldron, is usable by default ofcourse
+  /**
+   * Constructs a new Cauldron instance.
+   * By default, a newly created cauldron is in a usable state.
+   * * <p><b>Pre-conditions:</b> None.</p>
+   * <p><b>Post-conditions:</b> A new cauldron object is instantiated with its isUsable attribute set to true.</p>
+   */ 
+  public Cauldron() {
     this.isUsable = true;
   }
 
+  /**
+   * Checks the current operational status of the cauldron.
+   * * @return true if the cauldron can be used for brewing, false if otherwise
+   * <p><b>Pre-conditions:</b> The cauldron object must be created.</p>
+   * <p><b>Post-conditions:</b> The current usability state is returned. The state of the cauldron is unchanged</p>
+   */ 
   public boolean checkUsability() {
     return this.isUsable;
   }
 
-  public void ruinCauldron() {//detects the cauldron as unsable after a failed brew
+  /**
+   * Marks the cauldron as unusable, typically triggered after a failed brewing attempt
+   * * <p><b>Pre-conditions:</b> The cauldron object must be created.</p>
+   * <p><b>Post-conditions:</b> The cauldron will be unusable, its' isUsable attribute will be set to false.</p>
+   */   
+  public void ruinCauldron() {
     this.isUsable = false;
   }
 
-  public void blessCauldron() { //restores the cauldron so it can be usable again
+  /**
+   * Restores a ruined cauldron back to a functional, usable state.
+   * * <p><b>Pre-conditions:</b> The cauldron object must be created.</p>
+   * <p><b>Post-conditions:</b> The cauldrons' isUsable attribute will be reset to true.</p>
+   */ 
+  public void blessCauldron() {
     this.isUsable = true;
   }  
 }
