f582829ca5d943bdd996cfc53f2c22b4580da3b7
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-07-10 20:51:13 +0800

    Add main driver class for potion-brewing game

--- Game.java
+++ Game.java
@@ -1,1 +1,12 @@
+/**
+ * The main driver class for the potion-brewing game.
+ */
 
+public class Game {
+
+  public static void main(String[] args){
+
+    GameController gameController = new GameController();
+    gameController.startGame();
+  }
+}
