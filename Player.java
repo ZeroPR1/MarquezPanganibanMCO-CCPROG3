2504d57afb6cf4eae25a6fed117861b830317bde
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-07-10 11:44:59 +0800

    Enhance Player class documentation
    Added detailed documentation for the Player class and its constructor.

--- Player.java
+++ Player.java
@@ -3,12 +3,35 @@
  * * This file serves as the primary entity for a user in the game.
  * It manages the player's identity, currency, items and abilities.
  */
+
+/**
+ * Represents a player in the game
+ * This class encapsulates player details such as name, crystal balance,
+ * inventory of items and their spellbook.
+ */
 public class Player{
+  /** The display name of the player */
   private String name;
+
+  /** The current amount of in-game currency (crystals) the player has. */
   private int crystals;
+
+  /** The inventory system managing the players items */
   private Inventory inventory;
+
+  /** The spellbook managing the learnt spells of the player */
   private Spellbook spellbook;
 
+
+  /** 
+   * Constructs a new player with the specified name
+   * Creates the player with a starting balance of 5000 crystals,
+   * empty inventory and empty spellbook
+   *
+   * @param name The chosen name for the player
+   * * <p><b>Pre-conditions:</b> The provided name should be a valid, created String. </p>
+   * <p><b>Post-conditions:</b> A new Player object is created with the given name, 5000 crystals and a new Inventory and Spellbook
+   */
   public Player(String name){
     this.name = name;
     this.crystals = 5000;
