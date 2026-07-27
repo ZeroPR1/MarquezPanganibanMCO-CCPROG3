e1dd035f899fc4efd12edf4cc7340fcd3d38475c
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-07-28 03:55:01 +0800

    Add quantity field and management methods to Item
    Added quantity field and methods to manage it.

--- Item.java
+++ Item.java
@@ -4,6 +4,7 @@
 
 public abstract class Item {
   private String name;
+  private int quantity;
   private int buyPrice;
   private int sellPrice;
 
@@ -17,9 +18,41 @@ public abstract class Item {
 
   public item(String name, int buyPrice, int sellPrice) {
     this.name = name;
+    this.quantity = quantity;
     this.buyPrice = buyPrice;
     this.sellPrice = sellPrice;
   }
 
-  
+  public String getName() {
+    return name;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+  public int getBuyPrice() {
+    return buyPrice;
+  }
+
+  public int getSellPrice() {
+    return sellPrice;
+  } 
+
+  public void addQuantity(int amount) {
+    if (amount > 0) {
+      this.quantity += amount;
+    }
+  }
+
+  public boolean deductQuantity(int amount) {
+    boolean success = false;
+    if (amount > 0 && this.quantity >= amount) {
+      this.quantity -= amount;
+      success = true;
+    }
+    return success;
+  }
+
+  public abstract String getItemType();
 }
