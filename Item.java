fef8b9334e5cd85c3ecdd870b600ab8feb5ce974
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-07-28 03:43:07 +0800

    Create abstract Item class for game inventory
    Added an abstract class for game items with properties for inventory and market management.

--- Item.java
+++ Item.java
@@ -1,1 +1,26 @@
+/**
+ * Abstract superclass that represents a generic item in game
+ * Provides standard properties for inventory and market management
+ */
 
+public abstract class Item {
+  private String name;
+  private int buyPrice;
+  private int sellPrice;
+
+/**
+ * Constructor for intializing fthe core attributes of the item 
+ * 
+ * @param name: The name of the item
+ * @param buyPrice: The markets' purchase price in crystals
+ * @param sellPrice: The markets' selling price in crystals
+ */
+
+  public item(String name, int buyPrice, int sellprice) {
+    this.name = name;
+    this.buyPrice = buyPrice;
+    this.sellPrice = sellPrice;
+  }
+
+  
+}
