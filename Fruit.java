1fc38580a8d23510ba3c52bbe513a939a52d0d52
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-07-31 14:54:30 +0800

    Implement determineBuyPrice method in Fruit class
    Added determineBuyPrice method to calculate fruit prices based on name.

--- Fruit.java
+++ Fruit.java
@@ -6,6 +6,46 @@ public class Fruit extends Item {
   public Fruit(String name, int quantity) {
     super(name, quantity, determineBuyPrice(name), determineSellPrice(name));
   }
-  
-  
-}
+
+  private static int determineBuyPrice(String name) {
+    int price = 0;
+
+    switch (name.toUpperCase()){
+      case "STRAWBERRY":
+        price = 120;
+        break;
+      case "ORANGE":
+        price = 80;
+        break;
+      case "LEMON":
+        price = 50;
+        break;
+      case "BANANA":
+        price = 75;
+        break;
+      case "MANGO":
+        price = 90;
+        break;
+      case "PINEAPPLE":
+        price = 240;
+        break;
+      case "KIWI":
+        price = 200;
+        break;
+      case "BLUEBERRY":
+        price = 200;
+        break;
+      case "COCONUT":
+        price = 180;
+        break;
+      default:
+        price = 0;
+        break;
+    }
+    return price;
+  }
+    @Override
+      public String getItemTyipe() {
+      return "Fruit";
+      }
+  }
