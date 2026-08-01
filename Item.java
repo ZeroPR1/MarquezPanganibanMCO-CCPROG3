9f7ad4180197d39934c95bfde5d88639d07f2d2e
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-08-01 16:05:32 +0800

    Refactor Item class and improve constructor
    Refactored Item class by consolidating methods and fixing constructor.

--- Item.java
+++ Item.java
@@ -1,58 +1,49 @@
 /**
- * Abstract superclass that represents a generic item in game
+ * Abstract superclass representing a generic item in the game.
  */
-
 public abstract class Item {
-  private String name;
-  private int quantity;
-  private int buyPrice;
-  private int sellPrice;
+    private String name;
+    private int quantity;
+    private int buyPrice;
+    private int sellPrice;
 
-/**
- * Constructor for intializing fthe core attributes of the item 
- * 
- * @param name: The name of the item
- * @param buyPrice: The markets' purchase price in crystals
- * @param sellPrice: The markets' selling price in crystals
- */
-
-  public item(String name, int buyPrice, int sellPrice) {
-    this.name = name;
-    this.quantity = quantity;
-    this.buyPrice = buyPrice;
-    this.sellPrice = sellPrice;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public int getQuantity() {
-    return quantity;
-  }
-
-  public int getBuyPrice() {
-    return buyPrice;
-  }
-
-  public int getSellPrice() {
-    return sellPrice;
-  } 
-
-  public void addQuantity(int amount) {
-    if (amount > 0) {
-      this.quantity += amount;
+    public Item(String name, int quantity, int buyPrice, int sellPrice) {
+        this.name = name;
+        this.quantity = quantity;
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
     }
-  }
 
-  public boolean deductQuantity(int amount) {
-    boolean success = false;
-    if (amount > 0 && this.quantity >= amount) {
-      this.quantity -= amount;
-      success = true;
+    public String getName() { 
+    	return name; 
     }
-    return success;
-  }
+    
+    public int getQuantity() { 
+    	return quantity; 
+    }
+    
+    public int getBuyPrice() { 
+    	return buyPrice; 
+    }
+    
+    public int getSellPrice() { 
+    	return sellPrice; 
+    }
 
-  public abstract String getItemType();
+    public void addQuantity(int amount) {
+        if (amount > 0) {
+            this.quantity += amount;
+        }
+    }
+
+    public boolean deductQuantity(int amount) {
+        boolean success = false;
+        if (amount > 0 && this.quantity >= amount) {
+            this.quantity -= amount;
+            success = true;
+        }
+        return success;
+    }
+
+    public abstract String getItemType();
 }
